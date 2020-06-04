0bdb9a6965c0fb498ab569d40741137705d68801
Charles Klein <kleincharles312@gmail.com>
2020-06-04 15:43:30 -0400

    Added Logs Class

--- 19_Capstone/Capstone/Models/Logs.cs
+++ 19_Capstone/Capstone/Models/Logs.cs
@@ -1,21 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace Capstone.Models
 {
     public class Logs
     {
         private string aPath { get; }
-
+        
         private string sPath { get; }
+
+        private string itemPath { get; }
 
         public Logs()
         {
             aPath = "..\\..\\..\\LogsLocations\\Log.txt";
+            //TODO: format file name for log
+            sPath = $"..\\..\\..\\LogsLocations\\SalesReport";
 
-            sPath = $"..\\..\\..\\LogsLocations\\SalesReport_{DateTime.Now.ToString("'yyyy'-'mm'-'dd'T'HH''mm'")}";
+            itemPath = "..\\..\\..\\..\\vendingmachine.csv";
         }
 
+        public List<VendingItem> Load()
+        {
+            List<VendingItem> list = new List<VendingItem>();
+            if (!File.Exists(itemPath))
+            {
+                return list;
+            }
+            using (StreamReader rdr = new StreamReader(itemPath))
+            {
+                while (!rdr.EndOfStream)
+                {
+                    string line = rdr.ReadLine();
+                    string[] fields = line.Split("|");
+                    decimal cost = decimal.Parse(fields[2]);
+                    VendingItem item = new VendingItem(fields[0], fields[1], cost, fields[3]);
+                    list.Add(item);
+                } 
+            }
+            return list;
+        }
+
+        public void EndOfDay(List<string> salesLog, List<string> auditLog)
+        {
+            using (StreamWriter writer = new StreamWriter(aPath, true))
+            {
+                foreach(string line in auditLog)
+                {
+                    writer.WriteLine();
+                }
+            }
+            
+            using (StreamWriter salesWriter = new StreamWriter(sPath, false))
+            {
+                foreach(string line in salesLog)
+                {
+                    salesWriter.WriteLine();
+                }
+            }
+        }
     }
 }
